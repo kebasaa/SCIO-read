cb0898b65771fa9df0cf6f81a22dee55dda966c4
kebasaa <muellerjonathan@gmx.net>
2019-03-03 09:50:43 +0200

    Testing data analysis
    This code does not properly work. For example, the overallLength variable is not correct, it's too long

---
+++ testing-code/testing.py
@@ -0,0 +1,41 @@
+
+def hex2int(hexstr,bits):
+    value = int(hexstr,16)
+    if value & (1 << (bits-1)):
+        value -= 1 << bits
+    return value
+
+
+fname="./example-data/file1.txt"
+
+with open(fname) as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip().split() for x in content]
+
+# Define protocol
+data_msg = -70 # defines which message to look for
+
+restLength = 0
+
+for line in content:
+    if(hex2int(line[0],8) == 1):
+        print("is the first line")
+        if(hex2int(line[1],8) == data_msg):
+            print("data coming in")
+        overallLength = (hex2int(line[2],8) & 255) | ((hex2int(line[3],8) & 255) << 8)
+        print("Rest length: ", restLength)
+        print("Overall length: ",overallLength)
+        restLength = overallLength
+        startingByte = 5
+    else:
+        startingByte = 1
+    # Now read the data
+    #print("Package number: ", hex2int(line[0],8))
+    rest = min(20, overallLength + 5)
+    for i in range(startingByte, rest):
+        #print(hex2int(line[i],8))
+        restLength-=1;
+    if (restLength <= 0):
+        print("done")
+print("Rest length: ", restLength)
