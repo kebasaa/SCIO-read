91c9a8d8043db4d9f283556aabd40b50a2e3e082
kebasaa <muellerjonathan@gmx.net>
2019-08-30 13:38:26 +0300

    updaate

--- testing-code/collect_data_USB2.py
+++ testing-code/collect_data_USB2.py
@@ -115,6 +115,69 @@ def read_temperature(scio_dev):
     objectTemperature = message_data[2]
     return(cmosTemperature, chipTemperature, objectTemperature)
     
+    
+def read_data(scio_dev, command):
+    # This reads the temperature
+    #import protocol message definitions
+    # e.g. data = -70, temperature = 4, etc ################
+    import struct
+    
+    # Open serial connection
+    try:
+        ser = serial.Serial(scio_dev)
+    except OSError as error:
+        log.error(error)
+        quit()
+
+    def protocol_message(command):
+        byte_command = b"" # empty initialisation
+        if(command == "read_temperature"):  # TODO: take from imported protocol
+        # message needed to read the temperature
+        # I could even build the command from [1, -70, 4, 0, 0] by replacing -70 & 4 #################
+            byte_command = b"\x01\xba\x04\x00\x00"
+        return(byte_command)
+
+    msg = protocol_message(command)
+    # write the message to the serial device
+    ser.write(msg)
+
+    # Start reading the response
+    s = ser.read(1)
+    message_type    = struct.unpack('<b',s)[0]
+    if(message_type != -70):
+        log.debug("Wrong message type: " + str(message_type))
+        #raise error  #######################
+    s = ser.read(1)
+    message_content = struct.unpack('<b',s)[0]
+    if(message_content == 4):
+        log.debug("Receiving temperature data: " + str(message_content))
+    elif(message_content == 2):
+        log.debug("Receiving scan data: " + str(message_content))
+    else:
+        log.debug("Receiving unknown message: " + str(message_content))
+    s = ser.read(2)
+    message_length = struct.unpack('<H',s)[0]
+    log.debug("Number of bytes: " + str(message_length))
+    log.debug("Number of longs: " + str(message_length/4))
+    log.debug("Number of variables (5 bytes each): " + str(message_length/5))
+    # Read the number of values specified by the message length
+    s = ser.read(message_length) # Scan data consists of 3x these messages! #################### 
+    ser.close()
+    
+    num_vars = message_length / 4 # divide by 4 because we are dealing with longs
+    data_struct = '<' + str(int(num_vars)) + 'l' # This is '<3l' or '<lll'
+    print(data_struct)
+    # Convert bytes to unsigned int
+    print(s)
+    message_data = struct.unpack(data_struct ,s)
+    print(message_data)
+
+    # Convert to temperatures
+    cmosTemperature = (message_data[0] - 375.22) / 1.4092 # Does this make sense? It's from the disassembled Android app...
+    chipTemperature = (message_data[1] - 375.22) / 100 # The subtraction is added by me. Not true it should be there
+    objectTemperature = message_data[2]
+    return(cmosTemperature, chipTemperature, objectTemperature)
+    
 
 def main_fct(calibrate, input_method, outfile):
     if(calibrate):
@@ -123,25 +186,32 @@ def main_fct(calibrate, input_method, outfile):
     log.info("--> Input through:    " + input_method)
     log.info("--> Output file name: " + outfile)
     
-    # Check the platform
-    # https://dzone.com/articles/linux-system-mining-python
-    if(platform.uname().system != 'Linux'):
-        log.error("This program only works on Linux.")
-        log.error("--> You are running " + platform.uname().system + ".")
-        log.error("--> Exiting...")
-        quit()
-    
     def find_scio_dev():
         scio_dev = ""
-        for device in glob.glob('/dev/*'):
-            for pattern in ['ttyACM*']:
-                if re.compile(pattern).match(os.path.basename(device)):
-                    print('Device:           {0}'.format(device))
-                    scio_dev = device
+        # Check the platform
+        # https://dzone.com/articles/linux-system-mining-python
+        if(platform.uname().system == 'Linux'):
+            # TODO: Add check in case multiple /dev/ttyACMs ports are available #######################
+            for device in glob.glob('/dev/*'):
+                for pattern in ['ttyACM*']:
+                    if re.compile(pattern).match(os.path.basename(device)):
+                        print('Device:           {0}'.format(device))
+                        scio_dev = device
+        elif(platform.uname().system == 'Windows'):
+            # TODO: Add check in case multiple COM ports are available #######################
+            import serial.tools.list_ports as port_list
+            if(port_list.comports()):
+                scio_dev = port_list.comports()[0][0]
+        else:
+            log.error("This program currently only works on Linux & Windows.")
+            log.error("--> You are running " + platform.uname().system + ".")
+            log.error("--> Exiting...")
+            quit()
         if scio_dev == "":
-            print("If no ttyACM is detected: Is the SCIO on?")
+            print("If no ttyACM (Linux) or COM (Windows) is detected: Is the SCIO on?")
             quit()
         # TODO: Add a check or manufacturer to determine that this is really the scio and not some arduino
+        log.info("Using port: " + scio_dev)
         return(scio_dev)
     
     scio_device = find_scio_dev()
